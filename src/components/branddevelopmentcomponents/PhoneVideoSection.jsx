64914f49ff24fa909f329ccda85830649d6659d8
startupsquad-prog <admin@startupsquad.in>
2025-10-01 11:42:03 +0530

    Fix: iPhone.png case in PhoneVideoSection

--- src/components/branddevelopmentcomponents/PhoneVideoSection.jsx
+++ src/components/branddevelopmentcomponents/PhoneVideoSection.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Link } from "react-router-dom";
-import iphoneImg from "../../assets/images/iphone.png";
+import iphoneImg from "../../assets/images/iPhone.png";
 
 const PhoneVideoSection = ({onButtonClick}) => {
   useEffect(() => {
